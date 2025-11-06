efe1574a24086d55089bf8a54c27cd0c4ed89e5d
JasonCarmichael4 <231156368+JasonCarmichael4@users.noreply.github.com>
2025-11-05 23:32:17 -0600

    initial commit

---
+++ p0-project/src/main.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
